f97e9fb477de40a94bac7474006a967200a851d4
Rodo <rodonguyendd@gmail.com>
2022-08-03 22:53:20 +1000

    Add search bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import Patient from './components/patient.js';
+import Patient from './components/Patient';
+import SearchBar from './components/SearchBar';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Patient/>
+        <SearchBar placeholder="Enter your search input here..." />
+        <Patient />
+
 
 
         {/* <img src={logo} className="App-logo" alt="logo" />
---
+++ src/components/SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./SearchBar.css"
+import SearchIcon from "@material-ui/icons/Search"
+
+
+export default function SearchBar({placeholder}) {
+    return (
+        <div className='search'>
+            <div className='searchInput'>
+                <input type='text' placeholder={placeholder}/>
+                <div className='searchIcon'>
+                    <SearchIcon/>
+                </div>
+            </div>
+        </div>
+    )
+}
