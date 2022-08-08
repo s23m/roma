aec8b7deea9f25a506da68a50c9f1b3cb0873237
Max Goodwin <maxgoodwin3@pm.me>
2022-08-08 15:07:03 +1000

    Restructured folders, added and formatted with prettier

--- src/components/Patient.js
+++ src/components/Patient.js
@@ -1,87 +1,77 @@
-import React, { useState, useEffect } from "react"
-import axios from "axios"
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-// Details about test servers here: 
+// Details about test servers here:
 // https://www.notion.so/IPS-related-Servers-6acf8209fbfe484680c6a852b4292494
-const BASE_URL = 'http://nprogram.azurewebsites.net/'
-const PATIENT_URL = `${BASE_URL}Patient/`
-
-
-
+const BASE_URL = 'http://nprogram.azurewebsites.net/';
+const PATIENT_URL = `${BASE_URL}Patient/`;
 
 /**
  *  @description Search for patient by their ID number
- *  @param {integer} patientID 
+ *  @param {integer} patientID
  *  @returns {xxx} xxx
  */
-const queryPatient = async (patientID, format='json') => {
-    const fullUrl = `${PATIENT_URL}${patientID}?_format=${format}`
-    const response = await axios.get(fullUrl)
+const queryPatient = async (patientID, format = 'json') => {
+  const fullUrl = `${PATIENT_URL}${patientID}?_format=${format}`;
+  const response = await axios.get(fullUrl);
 
-    return response
-    // setPatientInfo(response.data.text.div)
-}
+  return response;
+  // setPatientInfo(response.data.text.div)
+};
 
 /**
  *  @description Search for patient by Date of Birth
- *  @param {string} DoB 
- *  Date of Birth in YYYY-MM-DD format   
+ *  @param {string} DoB
+ *  Date of Birth in YYYY-MM-DD format
  *  E.G. 2001-02-08
  *  @returns {xxx} xxx
  */
 const searchPatientByDoB = async (DoB) => {
-    const fullUrl = `${PATIENT_URL}search?birthDate=${DoB}&_format=json`
-    const response = await axios.get(fullUrl)
-}
+  const fullUrl = `${PATIENT_URL}search?birthDate=${DoB}&_format=json`;
+  const response = await axios.get(fullUrl);
+};
 
 /**
  *  @description Search for patient by name. Note that `name` can either be in their first or last name.
- *  @param {string} name 
+ *  @param {string} name
  *  @returns {xxx} xxx
  */
 const searchPatientByName = async (name) => {
-    const fullUrl = `${PATIENT_URL}search?name=${name}&_format=json`
-    const response = await axios.get(fullUrl)
+  const fullUrl = `${PATIENT_URL}search?name=${name}&_format=json`;
+  const response = await axios.get(fullUrl);
 
-    
-    console.log(response.data)
-}
-
+  console.log(response.data);
+};
 
 /**
  *  @description Search for patient by last name.
- *  @param {string} name 
+ *  @param {string} name
  *  @returns {xxx} xxx
  */
 const searchPatientByLastName = async (lastName) => {
-    const fullUrl = `${PATIENT_URL}search?family=${lastName}&_format=json`
-    const response = await axios.get(fullUrl)
-}
-
-
-
+  const fullUrl = `${PATIENT_URL}search?family=${lastName}&_format=json`;
+  const response = await axios.get(fullUrl);
+};
 
 /**
- *  @description Search for patient by name/last name/date of birth. 
+ *  @description Search for patient by name/last name/date of birth.
  *  Note that `name` can either be in their first or last name. Date of Birth is in YYYY-MM-DD format.
- *  @param {string} searchContent 
+ *  @param {string} searchContent
  *  @param {string} searchCategory
  *  Must be 'name, 'lastname' or 'dob'
  *  @returns {xxx} xxx
  */
-const searchPatient = async(searchContent, searchCategory) => {
-    if (searchCategory === 'name') {
-        searchPatientByName(searchContent)
-    } else if (searchCategory === 'lastname') {
-        searchPatientByLastName(searchContent)
-    } else if (searchCategory === 'dob') {
-        searchPatientByDoB(searchContent)
-    } else {
-        throw new Error("searchCategory is not valid. Must be 'name, 'lastname' or 'dob'.")
-    }
-}
-
-
+const searchPatient = async (searchContent, searchCategory) => {
+  if (searchCategory === 'name') {
+    searchPatientByName(searchContent);
+  } else if (searchCategory === 'lastname') {
+    searchPatientByLastName(searchContent);
+  } else if (searchCategory === 'dob') {
+    searchPatientByDoB(searchContent);
+  } else {
+    throw new Error("searchCategory is not valid. Must be 'name, 'lastname' or 'dob'.");
+  }
+};
 
 // queryPatient(1)
 // queryPatient(2)
@@ -91,21 +81,21 @@ const searchPatient = async(searchContent, searchCategory) => {
 // searchPatientByName('Bradley')
 
 export default function Patient() {
-    const [patientInfo, setPatientInfo] = useState()
+  const [patientInfo, setPatientInfo] = useState();
 
-    useEffect(() => {
-        queryPatient(1)
-        .then((response) => setPatientInfo(response.data.name[0].given + ' ' + response.data.name[0].family))
-    })
+  useEffect(() => {
+    queryPatient(1).then((response) =>
+      setPatientInfo(response.data.name[0].given + ' ' + response.data.name[0].family)
+    );
+  });
 
-    return(
-        <div>
-            <p>Patient info is displayed here. </p>
-            {patientInfo}
-        </div>
+  return (
+    <div>
+      <p>Patient info is displayed here. </p>
+      {patientInfo}
+    </div>
 
-
-        // {countries.map((country) => (
-        //     <option key={country}>{country}</option> ))}
-    )
+    // {countries.map((country) => (
+    //     <option key={country}>{country}</option> ))}
+  );
 }
