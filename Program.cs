38a8820f8c4d5dd046e09f5e4be444a51ad9e991
MacUniGit <79879489+MacUniGit@users.noreply.github.com>
2022-08-10 17:38:18 +1000

    More info Dialog. PatientNameSearch component

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<DialogService>();
 
 var app = builder.Build();
 
