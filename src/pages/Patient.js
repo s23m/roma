94a02aefc1e8abdcf75f9bc3c02ffdf934374cb5
Rodo <rodonguyendd@gmail.com>
2022-08-12 15:59:22 +1000

    Add more columns to patient table

--- src/pages/Patient.js
+++ src/pages/Patient.js
@@ -12,23 +12,37 @@ const convertSearchResultToRowData = (searchResults) => {
     return {
       givenNames: patient.resource.name[0].given.join(' '),
       familyName: patient.resource.name[0].family,
+      birthDate:  patient.resource.birthDate,
+      gender: patient.resource.gender,
+      id: patient.resource.id
     }
   })
   return rowData;
 };
 
 // More to be added from available params of http://hapi.fhir.org/baseR4/
-const searchTypes = ['name', 'family', 'birthdate', 'phone', 'gender'] 
+const searchTypes = [
+  'name', 'family', 'birthdate', 'phone', 'gender', 'family', 
+  'deceased', 'address-state', '_lastUpdated', 'link',
+  'language', 'add-country', 'death-date', 'phonetic',
+  'telecom', 'address-city', 'email', 'given', 'identifier',
+  'address', 'general-practitioner', '_security', 'active',
+  'address-postcode', '_filter', 'profile', '_tag', 'organization',
+  '_has', 'address-use', '_source', '_id', '_text', '_content', 
+] 
 
 const Patient = () => {
   // ag-grid-table variables
   const columnDefs = [
-    { headerName: "Given names", field: "givenNames", sortable: true, filter: true, minWidth: 100, maxWidth: 150},
-    { headerName: "Family name", field: "familyName", sortable: true, filter: true, minWidth: 100, maxWidth: 150},
+    { headerName: "Given names", field: "givenNames", sortable: true, filter: true, resizable: true},
+    { headerName: "Family name", field: "familyName", sortable: true, filter: true, resizable: true},
+    { headerName: "Birthdate", field: "birthDate", sortable: true, filter: true, resizable: true},
+    { headerName: "Gender", field: "gender", sortable: true, filter: true, resizable: true},
+    { headerName: "ID", field: "id", sortable: true, filter: true, resizable: true},
   ];
   const [rowData, setRowData] = useState([
-    {givenNames: "Adam", familyName: "ThisisAnExample"},
-    {givenNames: 'Abbey',familyName: 'Goodwin'}
+    {givenNames: "Adam", familyName: "ThisisAnExample", birthDate: "1970-05-06", gender: "male", id: 123126969},
+    {givenNames: 'Abbey',familyName: 'Goodwin', birthDate: "1979-09-06", gender: "male", id: 1777777}
   ])
 
   const onSearchSubmit = async (queryType, queryValue) => {
@@ -47,13 +61,14 @@ const Patient = () => {
     <div>
       <SearchBar placeholder={'Search a patient name'} onSubmit={onSearchSubmit} options={searchTypes}/>
 
-      <div className='ag-theme-balham-dark' style={{height: 400, width: 600}}> 
+      <div className='ag-theme-balham-dark' style={{height: '70vh', width: '50vw'}}> 
         <AgGridReact
           columnDefs={columnDefs}
           rowData={rowData}
           pagination={true}
           paginationPageSize={25}
-          resizable={true}
+          onGridReady={(params) => {
+            params.api.sizeColumnsToFit();}}
         />
       </div>
     </div>
