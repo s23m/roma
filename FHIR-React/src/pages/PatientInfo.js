6a0fcd3f0308f7c693bc283225280801705e52b4
Rodo <rodonguyendd@gmail.com>
2022-08-31 17:58:43 +1000

    Add extractContent() to return more readable content

--- FHIR-React/src/pages/PatientInfo.js
+++ FHIR-React/src/pages/PatientInfo.js
@@ -8,25 +8,55 @@ import 'ag-grid-community/styles/ag-theme-balham.css';
 import '../stylesheets/PatientInfo.css';
 
 /**
- * A function that checks the validity of the value before passing to Client side. 
- * It also handles the data differently if `dataType` is given. 
- * @param {*} data 
- * @param {string} dataType 
+ * Extract the keys & values of the object and return it in a string in a tree-like structure.
+ * @param {Object} object
+ * @param {string} indent
+ * @returns content
+ */
+const extractContent = (object, indent = '', content = '') => {
+  if (typeof object === 'object') {
+    Object.entries(object).forEach(([key, value]) => {
+      // To avoid printing numbers as keys when object is an Array
+      if (!Array.isArray(object)) {
+        content += indent + key + ':';
+      }
+      // If this is an object of length >=1 and its value is not string
+      // (Basically it's not a single simple value), add a \n
+      if (Object.keys(value).length >= 1 && typeof Object.entries(value)[0][1] !== 'string') {
+        content += '\n';
+      }
+      content = extractContent(value, indent + ' ', content);
+    });
+  } else {
+    // console.log(indent, object)
+    content += ' ' + object + '\n';
+  }
+
+  // content.replace('\n', <br/>)
+  // parser = new DOMParser(); 
+  // xmlDoc = parser.parseFromString(content,"text/xml");
+  // return xmlDoc;
+  return content;
+};
+
+
+/**
+ * A function that checks the validity of the value before passing to Client side.
+ * It also handles the data differently if `dataType` is given.
+ * @param {*} data
+ * @param {string} dataType
  * @returns value
- */ 
-const getValue = (data, dataType=undefined) => {
-  if (dataType === 'communication' && data !== undefined) {
-    var communication = [];
-    data.forEach(
-      (element) => (communication = communication.concat([element.language.coding[0].display])));
-    return JSON.stringify(communication)
-  }
+ */
+const getValue = (data, dataType = undefined) => {
   if (data !== undefined) {
-    if (typeof data === 'object') return JSON.stringify(data)
-    return data
+    if (typeof data === 'object') {
+      console.log(extractContent(data));
+      return extractContent(data);
+    }
+    return data;
   }
-  return 'n/a'
-}
+  return 'n/a';
+};
 
 const convertData = (patientData) => {
   // See patient data structure here: https://www.hl7.org/fhir/patient.html#patient
@@ -36,17 +66,17 @@ const convertData = (patientData) => {
     { dataType: 'name', value: getValue(patientData.name) },
     { dataType: 'telecom', value: getValue(patientData.telecom) },
     { dataType: 'gender', value: getValue(patientData.gender) },
-    { dataType: 'birthDate', value:  getValue(patientData.birthDate) },
-    { dataType: 'deceased', value:  getValue(patientData.deceased) },
+    { dataType: 'birthDate', value: getValue(patientData.birthDate) },
+    { dataType: 'deceased', value: getValue(patientData.deceased) },
     { dataType: 'address', value: getValue(patientData.address) },
-    { dataType: 'maritalStatus', value:  getValue(patientData.maritalStatus) },
-    { dataType: 'multipleBirth', value:  getValue(patientData.multipleBirth) },
-    { dataType: 'photo', value:  getValue(patientData.photo) },
-    { dataType: 'contact', value:  getValue(patientData.contact) },
-    { dataType: 'communication', value: getValue(patientData.communication, 'communication') },
+    { dataType: 'maritalStatus', value: getValue(patientData.maritalStatus) },
+    { dataType: 'multipleBirth', value: getValue(patientData.multipleBirth) },
+    { dataType: 'photo', value: getValue(patientData.photo) },
+    { dataType: 'contact', value: getValue(patientData.contact) },
+    { dataType: 'communication', value: getValue(patientData.communication) },
     // { dataType: 'meta', value: getValue(JSON.stringify(patientData.meta)) }, // Do we need to display meta?
     { dataType: 'resourceType', value: getValue(patientData.resourceType) },
-  ];  
+  ];
 };
 
 const PatientInfo = () => {
@@ -54,19 +84,18 @@ const PatientInfo = () => {
   // ag-grid-table variables
   const [rowData, setRowData] = useState([]);
   const gridStyle = useMemo(() => ({ height: '60vh', width: '60vw' }), []);
-  const defaultColDef = { 
-    filter: true, 
+  const defaultColDef = {
+    filter: true,
     resizable: true,
-    wrapText: true, 
+    wrapText: true,
     autoHeight: true,
     autoWidth: true,
     editable: true,
   };
   const columnDefs = [
-    { headerName: 'Data type', field: 'dataType', maxWidth: 200},
+    { headerName: 'Data type', field: 'dataType', maxWidth: 200 },
     { headerName: 'Value', field: 'value' },
   ];
-
 
   useEffect(() => {
     getPatient(id).then((response) => {
@@ -75,7 +104,6 @@ const PatientInfo = () => {
       setRowData(data);
     });
   }, [id]);
-
 
   return (
     <div>
@@ -93,7 +121,7 @@ const PatientInfo = () => {
       </div>
 
       <h4>Allergy Intolerance</h4>
-      <AllergyIntolerance patientID={id}/>
+      <AllergyIntolerance patientID={id} />
     </div>
   );
 };
