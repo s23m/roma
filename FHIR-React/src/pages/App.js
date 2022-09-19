e654c1afcd1c4acf14d299a39584a6ed634ff270
Max Goodwin <maxgoodwin3@pm.me>
2022-09-19 14:57:56 +1000

    Fixed navbar error and added to .gitignore

--- FHIR-React/src/pages/App.js
+++ FHIR-React/src/pages/App.js
@@ -1,16 +1,15 @@
 import Patients from '../pages/Patients';
 import PatientInfo from '../pages/PatientInfo';
-import NavBar from '../components/NavBar';
+import SiteNavBar from '../components/SiteNavBar';
 import { Container } from 'reactstrap';
 import { Outlet, BrowserRouter, Routes, Route } from 'react-router-dom';
 import '../stylesheets/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 function App() {
   const AppConstantElements = (
     <div className="App">
-      <NavBar />
+      <SiteNavBar />
       <Container>
         <Outlet></Outlet>
       </Container>
