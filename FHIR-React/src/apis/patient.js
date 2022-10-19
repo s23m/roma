49fb34ea26f56c6826166d80f2bbd587873af73b
Max Goodwin <maxgoodwin3@pm.me>
2022-10-19 21:31:37 +1000

    Updated comments

--- FHIR-React/src/apis/patient.js
+++ FHIR-React/src/apis/patient.js
@@ -7,22 +7,25 @@ const PATIENT_URL = `${BASE_URL}Patient`;
 
 /**
  * Get patient search results from a set of parameter values
- * @param {Array} queryTypes 
- * @param {Array} queryValues 
+ * Each element in queryTypes should have a matching value in queryValues
+ * Matching types and values should be at the same index
+ * @param {Array<String>} queryTypes
+ * @param {Array<String>} queryValues
  * @returns response data
  */
 export const searchPatient = async (queryTypes, queryValues) => {
   if (queryTypes.length !== queryValues.length)
-  throw new Error('Invalid query. Unequal number of query types and query values.');
-  
-  // Construct fullUrl by by appending given parameter keys and values
+    throw new Error('Invalid query. Unequal number of query types and query values.');
+
+  // Construct query portion of request URL
+  // by appending each provided parameter key and value
   const query = queryTypes.reduce(
     (queryStr, queryType, i) =>
       !!queryType ? `${queryStr}${queryType}=${queryValues[i]}&` : queryStr,
     ''
   );
 
-  // Request and return response data 
+  // Request and return response data
   const fullUrl = `${PATIENT_URL}?${query}_format=json&_count=100`;
   const response = await axios.get(fullUrl);
   return response.data;
@@ -30,16 +33,16 @@ export const searchPatient = async (queryTypes, queryValues) => {
 
 /**
  * Get a patient's basic data from their ID
- * @param {integer} id 
+ * @param {integer} id
  * @returns response data
  */
 export const getPatient = async (id) => {
-  if (id === joeBlowId) return joeBlow; // Return a sample patient's data
-  
+  if (id === joeBlowId) return joeBlow; // Return mock patient's data
+
   // Construct fullUrl by appending patient's ID
   const fullUrl = `${PATIENT_URL}/${id}`;
-  
-  // Request and return response data 
+
+  // Request and return response data
   const response = await axios.get(fullUrl);
   return response.data;
 };
