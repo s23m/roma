bd9aadfa20f9eabe218f617c1901621b74424ae7
Max Goodwin <maxgoodwin3@pm.me>
2022-11-06 20:54:53 +1000

    Changed server to https

--- FHIR-React/src/apis/baseUrl.js
+++ FHIR-React/src/apis/baseUrl.js
@@ -1,10 +1,9 @@
 // Details about test servers here:
 // https://confluence.hl7.org/display/FHIR/Public+Test+Servers
 
-
 /**
  * BASE_URL: used to define a common server for other functions to use
  */
-// const BASE_URL = 'https://server.fire.ly/'; // Backup test server 
-const BASE_URL = 'http://hapi.fhir.org/baseR4/'  // Documentation: https://hapi.fhir.org/baseR4/swagger-ui/ 
+// const BASE_URL = 'https://server.fire.ly/'; // Backup test server
+const BASE_URL = 'https://hapi.fhir.org/baseR4/'; // Documentation: https://hapi.fhir.org/baseR4/swagger-ui/
 export default BASE_URL;
